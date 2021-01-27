dd9bfdb8d53677d6c35d9a642570b48191ed4ddf
sanya473 <51734865+sanya473@users.noreply.github.com>
2021-01-27 22:20:51 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
+    (function ($) {
+    "use strict"; 
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
